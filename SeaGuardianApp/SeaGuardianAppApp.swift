38b83606a8fa798b7ce9c38e3387da325bfdf2ae
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2025-02-24 11:43:39 +0100

    Initial Commit

---
+++ SeaGuardianApp/SeaGuardianAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaGuardianAppApp.swift
+//  SeaGuardianApp
+//
+//  Created by Andreas Virkesdal Jonsterhaug on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SeaGuardianAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
