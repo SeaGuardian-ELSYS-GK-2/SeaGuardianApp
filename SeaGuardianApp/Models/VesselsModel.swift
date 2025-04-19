8d21d3b51b67cc9a191ec76d3fbdab083371a848
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2025-04-19 14:01:53 +0200

    display crew on map

--- SeaGuardianApp/Models/VesselsModel.swift
+++ SeaGuardianApp/Models/VesselsModel.swift
@@ -3,16 +3,6 @@ import Foundation
 @Observable
 class VesselsModel {
     var vessels: [String: Vessel] = [:]
-    
-    static var preview: VesselsModel {
-        let model = VesselsModel()
-        model.vessels = [
-            "1": .init(id: "1", timestamp: 1744729028.494117, latitude: 63.44, longitude: 10.42),
-            "2": .init(id: "2", timestamp: 1744729029.494117, latitude: 64.00, longitude: 11.00),
-            "3": .init(id: "3", timestamp: 1744729030.494117, latitude: 65.00, longitude: 12.00),
-        ]
-        return model
-    }
 }
 
 struct Vessel: Identifiable {
@@ -30,4 +20,45 @@ struct CrewMember: Identifiable {
     var overBoard: Bool
     var latitude: Double?
     var longitude: Double?
+}
+
+
+extension VesselsModel {
+    static var preview: VesselsModel {
+        let model = VesselsModel()
+
+        model.vessels["1"] = Vessel(
+            id: "1",
+            timestamp: Date().timeIntervalSince1970,
+            latitude: 63.4705,
+            longitude: 10.3951,
+            crew: [
+                "crew_1": CrewMember(id: "crew_1", name: "Alice", overBoard: false, latitude: nil, longitude: nil),
+                "crew_2": CrewMember(id: "crew_2", name: "Bob", overBoard: true, latitude: 63.4715, longitude: 10.4192)
+            ]
+        )
+
+        model.vessels["2"] = Vessel(
+            id: "2",
+            timestamp: Date().timeIntervalSince1970,
+            latitude: 65.446500,
+            longitude: 11.421500,
+            crew: [
+                "crew_1": CrewMember(id: "crew_1", name: "Knut", overBoard: true, latitude: 65.2315, longitude: 11.3392),
+                "crew_2": CrewMember(id: "crew_2", name: "Roger", overBoard: true, latitude: 65.4315, longitude: 11.5292)
+            ]
+        )
+
+        model.vessels["3"] = Vessel(
+            id: "3",
+            timestamp: Date().timeIntervalSince1970,
+            latitude: 64.447200,
+            longitude: 9.820900,
+            crew: [
+                "crew_1": CrewMember(id: "crew_1", name: "James", overBoard: false, latitude: nil, longitude: nil)
+            ]
+        )
+
+        return model
+    }
 }
--- SeaGuardianApp/Views/Map/SeaGuardianMap.swift
+++ SeaGuardianApp/Views/Map/SeaGuardianMap.swift
@@ -10,10 +10,23 @@ struct SeaGuardianMap: View {
             Map(position: $cameraPosition) {
                 ForEach(vessels.vessels.values.sorted(by: { $0.id < $1.id })) { vessel in
                     Annotation("Vessel \(vessel.id)", coordinate: CLLocationCoordinate2D(latitude: vessel.latitude, longitude: vessel.longitude)) {
-                            Image(systemName: "ferry.fill")
-                                .resizable()
-                                .frame(width: 32, height: 32)
+                        Image(systemName: "ferry.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                    }
+
+                    ForEach(vessel.crew.values.filter { $0.overBoard }) { crew in
+                        if let lat = crew.latitude, let lng = crew.longitude {
+                            Annotation(crew.name, coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lng)) {
+                                VStack {
+                                    Image(systemName: "figure.pool.swim")
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                        .foregroundColor(.red)
+                                }
                             }
+                        }
+                    }
                 }
             }
             .mapStyle(.standard)
